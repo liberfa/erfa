2d67940e1372dd01a13882c171ba1155c8785523
Sergio Pascual <sergiopr@fis.ucm.es>
2016-12-02 17:29:35 +0100

    Expose MAJOR.MINOR.MICRO version numbers, use 'era' as prefix

--- src/erfaversion.c
+++ src/erfaversion.c
@@ -1,7 +1,22 @@
 #include <config.h>
 
-const char* erfa_version() {
+const char* eraVersion() {
 
   return PACKAGE_VERSION;
 
+}
+
+int eraVersionMajor() {
+
+  return PACKAGE_VERSION_MAJOR;
+}
+
+int eraVersionMinor() {
+
+  return PACKAGE_VERSION_MINOR;
+}
+
+int eraVersionMicro() {
+
+  return PACKAGE_VERSION_MICRO;
 }
