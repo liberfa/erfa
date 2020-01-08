fc66f264acc1582ecbbf843c2eb36a7a1c8ee3da
Adam Sawicki <adam.sawicki@ibm.com>
2020-01-08 01:24:50 -0500

    Update version information to be 1.7

--- src/erfaversion.c
+++ src/erfaversion.c
@@ -7,7 +7,7 @@
 */
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.6.0"
+#define PACKAGE_VERSION "1.7.0"
 
 /* Define to the major version of this package. */
 #define PACKAGE_VERSION_MAJOR 1
@@ -16,7 +16,7 @@
 #define PACKAGE_VERSION_MICRO 0
 
 /* Define to the minor version of this package. */
-#define PACKAGE_VERSION_MINOR 6
+#define PACKAGE_VERSION_MINOR 7
 
 /* Define to the version of SOFA */
 #define SOFA_VERSION "20190722"
